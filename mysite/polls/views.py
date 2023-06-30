d0b3b3d9d9a23e4fac5a08a765bb72d4daf88f1b
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-06-30 07:07:14 +0300

    Using list comprehension and django's database api
    * latest_question_list = Question.objects.order_by("-pub_date")[:5]
    
    This line queries the Question model (which represents a database table) to retrieve the latest 5 questions. The objects attribute represents the manager for the Question model, and order_by("-pub_date") orders the questions in descending order by their pub_date field.
    
    output = ", ".join([q.question_text for q in latest_question_list])
    
    Here, a list comprehension is used to extract the question_text attribute from each Question object in the latest_question_list. The resulting texts are then joined together with a comma , as the separator using the join method. This generates the content for the HTTP response.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from .models import Question
 
-
-# first the user is going to http://127.0.0.1:8000/polls/99/ for example in the browser
-# since he asks to go to polls/ it gets redirected to polls.urls
-# when we are in polls.urls we see that there is <int:question_id> goes right after polls/
-# user has specified that 99 is the <int:question_id>
-# since there is nothing else after <int:question_id> , it means that urls will redirect us to views.detail view
-# detail view as you can see here TAKES request and question_id(we got it from user - > urls -> views)
-# now we just render the page with some text and question_id at the back
-# In this specific example, %s is used as a placeholder for the question_id variable. 
-# When the string response is formatted using the % operator, the %s is replaced with the value of question_id.
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    print(latest_question_list)
+    output = ", ".join([q.question_text for q in latest_question_list])
+    print(output)
+    return HttpResponse(output)
+
+# def index(request):
+#     return HttpResponse("Hello world. You're at the polls index.")
 
 
 def detail(request, question_id):
